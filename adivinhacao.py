552ff91efc1ea3885c7ec807b1479f106bb408bd
Marcos <marcs.vdp@gmail.com>
2022-03-15 23:50:36 -0300

    a base para escolher qual dos jogos sera jogado usando import e function

---
+++ adivinhacao.py
@@ -0,0 +1,59 @@
+import random
+
+def jogar():
+    print("Jogo da Adivinhação")
+    print("Adivinhe o numero")
+
+    total_de_tentativas = 0
+    numero_random = random.randrange(1,101)
+    pontos = 1000
+
+    print("Escolha a dificuldade do jogo")
+    print("(1) Fácil (2) Médio (3) Dificil")
+    nivel = int(input("Defina um nivel: "))
+
+    if nivel == 1:
+        total_de_tentativas = 20
+    elif nivel == 2:
+        total_de_tentativas = 10
+    else:
+        total_de_tentativas = 5
+
+# esse mais é por que no range o numero limite não entra na contagem, no caso, o 3 do numero de tentativas não entra, então, adicionando mais um, temos o ajuste
+#range(start, stop, [step])
+
+    for rodada in range(1, total_de_tentativas + 1):   #for e a função range para não precisar delimitar um valor pra rodada e sim colocar um limite pro loop
+        print("Tentativa {} de {}".format(rodada, total_de_tentativas))  #interpolação de strings
+        chute_str = input("Digite o seu número entre 1 e 100: ")
+        print("Você digitou: ", chute_str)
+        chute = int(chute_str)
+
+        acertou = numero_random == chute
+        maior = chute > numero_random
+        menor = chute < numero_random
+
+        if chute < 1 or chute > 100:
+            print("Escolha um número entra 1 e 100")
+            continue
+
+        elif (acertou):
+            print("Você acertou e fez {} pontos!".format(pontos))
+            break
+        else:
+            if (maior):
+                print("Você errou! O seu chute foi maior que o número secreto.")
+            if (rodada == total_de_tentativas):
+                print("O numero secreto era {}. Você fez {}".format(numero_random, pontos))
+            elif (menor):
+                print("Você errou! O seu chute foi menor que o número secreto.")
+                if (rodada == total_de_tentativas):
+                    print("O numero secreto era {}. Você fez {}".format(numero_random, pontos))
+            pontos_perdidos = abs(numero_random - chute)  #abs para ter apenas numero absolutos
+            pontos = pontos - pontos_perdidos
+
+
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
--- forca.py
+++ forca.py
@@ -1,9 +1,31 @@
-
 # forca.py
 
 def jogar():
-    print("Jogo da forca")
-    print("Seja bem vindoe boa sorte")
+    print("**************************")
+    print("Seja bem vindo e boa sorte")
+
+    palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
+    enforcou = False
+    acertou = False
+
+    print("A palavra é " + str(letras_acertadas))
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual Letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+               letras_acertadas[index] = letra
+            index = index + 1
+        print(letras_acertadas)
+
+    print("Fim do jogo ")
 
 if(__name__ == "__main__"):
     jogar()
+
