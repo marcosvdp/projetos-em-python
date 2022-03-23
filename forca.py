543ceecec08409a5244bc3024c0d89a62abb6ef8
Marcos <marcs.vdp@gmail.com>
2022-03-22 23:27:50 -0300

    a base para escolher qual dos jogos sera jogado usando import e function.
    Definido um banco de palavras e a aleatoriedade da escolha dentro dele, comecei a organizar melhor o codigo usando functions

--- forca.py
+++ forca.py
@@ -1,31 +1,65 @@
-# forca.py
+import random
 
 def jogar():
-    print("**************************")
-    print("Seja bem vindo e boa sorte")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    boas_vindas()
 
+    palavra_secreta =  escolha_da_palavra()
+
+    letras_acertadas = ['_' for letra in palavra_secreta]
+
+    erros = 0
     enforcou = False
     acertou = False
 
     print("A palavra é " + str(letras_acertadas))
+    print("Você tem 6 tentativas, boa sorte!")
 
     while(not enforcou and not acertou):
 
         chute = input("Qual Letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-               letras_acertadas[index] = letra
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Erros: {}".format(erros))
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
+    if(acertou):
+        print("Você ganhou, parabéns!")
+    else:
+        print("Você perdeu! :(")
     print("Fim do jogo ")
+
+
+def boas_vindas():
+    print("**************************")
+    print("Seja bem vindo e boa sorte")
+    print("**************************")
+
+def escolha_da_palavra():
+    arquivo = open("banco de palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
 
 if(__name__ == "__main__"):
     jogar()
-
